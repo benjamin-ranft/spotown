e0a60c93232e42fd346a81869f27b5c54f5bef6a
benjaminranft <hello@benjaminranft.com>
2020-11-13 11:49:25 +0100

    Initial Spring Project Setup

---
+++ src/main/java/de/benjaminranft/finalproject/FinalprojectApplication.java
@@ -0,0 +1,13 @@
+package de.benjaminranft.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/benjaminranft/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.benjaminranft.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
