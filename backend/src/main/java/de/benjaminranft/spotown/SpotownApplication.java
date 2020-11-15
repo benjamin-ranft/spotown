24a50a8183f862fdf09aaa7d03042a5a29144ec1
benjaminranft <hello@benjaminranft.com>
2020-11-15 16:06:49 +0100

    Rebuild and add frontend/backend and project structure

---
+++ backend/src/main/java/de/benjaminranft/spotown/SpotownApplication.java
@@ -0,0 +1,13 @@
+package de.benjaminranft.spotown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotownApplication.class, args);
+	}
+
+}
