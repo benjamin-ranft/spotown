eb3efe09917c2d2d50f923eb18636aa31330551a
benjaminranft <hello@benjaminranft.com>
2020-11-16 10:49:32 +0100

    Fix Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import LoginPage from "./Components/LoginPage";
-import {Redirect , Route} from "react-router-dom";
+import {Redirect , Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div>
       Welcome
-        <switch>
+        <Switch>
             <Route path="/login" component={LoginPage}/>
             <Route path="/">
                 <Redirect to="/login"/>
             </Route>
-        </switch>
+        </Switch>
     </div>
   );
 }
