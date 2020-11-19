bc9e0b273f360ee5ed62bc95969694cae6ffdfdf
benjaminranft <hello@benjaminranft.com>
2020-11-19 15:05:43 +0100

    Remove unused test

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-xtest('renders Welcome', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome/i);
-  expect(linkElement).toBeInTheDocument();
-});
