647ae77abecd836832a7d50f9e21a7766ed9002f
benjaminranft <hello@benjaminranft.com>
2020-12-09 12:46:25 +0100

    Fix CLI problem (unused React import)

--- frontend/src/components/utils/useCopyToClipboard.js
+++ frontend/src/components/utils/useCopyToClipboard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from "react";
+import {useState, useEffect, useCallback} from "react";
 import copy from "copy-to-clipboard";
 
 export default function useCopyToClipboard(resetInterval = null) {
